b120c4996fc9d4efbf3c5bdbe49c1d957120a6f8
Jihun Park <gladsuve@gmail.com>
2022-05-13 16:02:44 +0900

    frontend

---
+++ DAY03/lab0301/poly.js
@@ -0,0 +1,12 @@
+// 토글 버튼의 가져오는 객체
+const toggleBtn = document.querySelector(".navbar_toggleBtn");
+// 메뉴를 가져오는 객체
+const menu = document.querySelector(".navbar_menu");
+// 아이콘을 가져오는 객체
+const icons = document.querySelector(".navbar_icons");
+
+//이벤트를 사용하려면 객체를 만들고 이벤트 리스너를 붙어야 사용이 가능
+toggleBtn.addEventListener("click", ()=>{
+    menu.classList.toggle('active');
+    icons.classList.toggle('active');
+})
